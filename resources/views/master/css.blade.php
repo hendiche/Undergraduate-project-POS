a9a808dbb493b3ab45ca29c8e5c431b0aee5b57c
Unknown <talles_of_destiny@yahoo.com.sg>
2017-12-02 18:38:05 +0700

    init commit and added template

---
+++ resources/views/master/css.blade.php
@@ -0,0 +1,12 @@
+    <!-- Bootstrap Core CSS -->
+    {!! Html::style('assets/vendor/bootstrap/css/bootstrap.min.css') !!}
+    <!-- MetisMenu CSS -->
+    {!! Html::style('assets/vendor/metisMenu/metisMenu.min.css') !!}
+    <!-- Morris Charts CSS -->
+    {!! Html::style('assets/vendor/morrisjs/morris.css') !!}
+    <!-- Custom Fonts -->
+    {!! Html::style('assets/vendor/font-awesome/css/font-awesome.min.css') !!}
+    <!-- Custom CSS -->
+    {!! Html::style('assets/dist/css/sb-admin-2.css') !!}
+    <!--date-->
+    {!! Html::style('assets/date/bootstrap-datepicker3.min.css') !!}
---
+++ resources/views/master/js.blade.php
@@ -0,0 +1,15 @@
+    <!-- jQuery -->
+    {!! Html::script('assets/vendor/jquery/jquery.min.js') !!}
+    <!-- Bootstrap Core JavaScript -->
+    {!! Html::script('assets/vendor/bootstrap/js/bootstrap.min.js') !!}
+    <!-- Metis Menu Plugin JavaScript -->
+    {!! Html::script('assets/vendor/metisMenu/metisMenu.min.js') !!}
+    <!-- Morris Charts JavaScript -->
+    {!! Html::script('assets/vendor/raphael/raphael.min.js') !!}
+    {!! Html::script('assets/vendor/morrisjs/morris.min.js') !!}
+    {!! Html::script('assets/data/morris-data.js') !!}
+    <!-- Custom Theme JavaScript -->
+    {!! Html::script('assets/dist/js/sb-admin-2.js') !!}
+    {!! Html::script('assets/date/bootstrap-datepicker.min.js') !!}
+    <!--jquery-->
+    {!! Html::script('assets/date/js/jquery-3.2.1.js') !!}
