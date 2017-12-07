86f03a2fabf2b3293c08d4e547e9b1991e68049c
harsanto <harsz.zhang@gmail.com>
2017-12-07 14:44:13 +0700

    add admin panel base

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Yajra\Datatables\DatatablesServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
