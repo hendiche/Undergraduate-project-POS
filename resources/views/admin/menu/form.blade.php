e500b90f0d1623ec58473e42d9073ad26c55e129
harsanto <harsz.zhang@gmail.com>
2017-12-10 05:57:31 +0700

    add maxlength validation to menu name

--- resources/views/admin/menu/form.blade.php
+++ resources/views/admin/menu/form.blade.php
@@ -57,7 +57,7 @@ Menu Form
     <div class="form-group">
         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Name</label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            {{ Form::text('name', null, ['class' => 'form-control col-md-7 col-xs-12', 'placeholder' => 'Name','required'=>'required']) }}
+            {{ Form::text('name', null, ['class' => 'form-control col-md-7 col-xs-12', 'placeholder' => 'Name','required'=>'required','maxlength'=>30]) }}
         </div>
     </div>
     <div class="form-group">
